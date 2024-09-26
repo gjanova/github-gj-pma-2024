968f9d6462ca3b9b46783a152368ba9c49d0e253
Gabriela Jánová <gabriela.janova@tul.cz>
2024-09-26 15:24:32 +0200

    #1 Vytvoření lineárního layoutu

--- MyApp001LinearLayout/app/src/main/java/com/example/myapp001linearlayout/MainActivity.kt
+++ MyApp001LinearLayout/app/src/main/java/com/example/myapp001linearlayout/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapp001linearlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,53 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        //enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        } */
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etLastName = findViewById<EditText>(R.id.etLastName)
+        val etPlace = findViewById<EditText>(R.id.etPlace)
+        val etVek = findViewById<EditText>(R.id.etVek)
+        val tvInfo = findViewById<TextView>(R.id.tvInfo)
+        val BtnSend = findViewById<Button>(R.id.BtnSend)
+        val BtnDelete = findViewById<Button>(R.id.BtnDelete)
+
+        //Nastavení obsluhy tlačítka Odeslat
+
+        BtnSend.setOnClickListener {
+            val name = etName.text.toString()
+            val lastname = etLastName.text.toString()
+            val place = etPlace.text.toString()
+            val vek = etVek.text.toString()
+
+            //Zobrazení textu v TextView
+
+            val formattedText = "Jmenuji se $name $lastname. Moje bydliště je $place. Je mi $vek let."
+            tvInfo.text = formattedText
         }
+
+        //Nastevení obsluhy tlačítka Smazat
+
+        BtnDelete.setOnClickListener {
+            etName.text.clear()
+            etLastName.text.clear()
+            etPlace.text.clear()
+            etVek.text.clear()
+
+            //Zobrazení textu v TextView
+
+            tvInfo.text = ""
+
+        }
+
     }
 }

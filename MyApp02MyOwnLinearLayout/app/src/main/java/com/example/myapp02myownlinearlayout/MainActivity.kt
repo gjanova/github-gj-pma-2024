86fc6916f77c7957d156a052fb585544430d0fe1
Gabriela Jánová <gabriela.janova@tul.cz>
2024-09-26 16:02:49 +0200

    #2

--- MyApp02MyOwnLinearLayout/app/src/main/java/com/example/myapp02myownlinearlayout/MainActivity.kt
+++ MyApp02MyOwnLinearLayout/app/src/main/java/com/example/myapp02myownlinearlayout/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapp02myownlinearlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,25 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+
+        val etVysledek = findViewById<EditText>(R.id.etVysledek)
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val tvVysledek = findViewById<TextView>(R.id.tvVysledek)
+
+                btnSend.setOnClickListener {
+            val vysledek = etVysledek.text.toString()
         }
+        val formattedText = "Výsledek je 20."
+        tvVysledek.text = formattedText
+
     }
 }

474e958a8cb8a2fae5767865dee20d2d986c64c1
Gabriela Jánová <gabriela.janova@tul.cz>
2024-10-10 10:48:19 +0200

    #3 dokončení

--- MyApp003Objednavka/app/build.gradle.kts
+++ MyApp003Objednavka/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.ViewBinding
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -33,6 +35,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+buildFeatures{
+    viewBinding = true
+}
+
 }
 
 dependencies {
--- MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
+++ MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
@@ -5,16 +5,56 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.example.myapp003objednavka.databinding.ActivityMainBinding
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //enableEdgeToEdge()
+        //binding settings
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    title = "Objednávka jídla"
+
+    binding.btnObjednat.setOnClickListener{
+        //načtení ID vybraného radiobuttonu z radiogroup
+        val testovinyRbId = binding.rgTestoviny.checkedRadioButtonId
+        val testoviny = findViewById<RadioButton>(testovinyRbId)
+
+        val polevka = binding.cbPolevka.isChecked
+        val dezert = binding.cbDezert.isChecked
+        val piti = binding.cbPiti.isChecked
+
+        val objednavkaText = "Souhrn objednávky: " +
+                "$testoviny.text" +
+                (if(polevka)"; polevka" else "") +
+                (if(dezert)"; dezert" else "") +
+                (if(piti)"; piti" else "")
+
+                    binding.tvSouhrn.text = objednavkaText
+
+    }
+
+        //změna obrázku v závislosti na vybraném radiobuttonu
+
+        binding.rbTestoviny1.setOnClickListener{
+            binding.ivTestoviny.setImageResource(R.drawable.kremove_rajcatove_testoviny)
         }
+
+        binding.rbTestoviny2.setOnClickListener{
+            binding.ivTestoviny.setImageResource(R.drawable.mac_and_cheese)
+        }
+
+        binding.rbTestoviny3.setOnClickListener{
+            binding.ivTestoviny.setImageResource(R.drawable.zapecene_testoviny_se_smetanou)
+        }
+
+
     }
 }

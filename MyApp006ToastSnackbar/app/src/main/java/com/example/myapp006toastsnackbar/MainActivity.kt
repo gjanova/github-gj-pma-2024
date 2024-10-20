6766ed30f4fbc7ea8a2ce496862d81647cdfe5a6
Gabriela <gabriela.janova@tul.cz>
2024-10-20 17:40:55 +0200

    #6 dokončení

--- MyApp006ToastSnackbar/app/src/main/java/com/example/myapp006toastsnackbar/MainActivity.kt
+++ MyApp006ToastSnackbar/app/src/main/java/com/example/myapp006toastsnackbar/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapp006toastsnackbar
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapp006toastsnackbar.databinding.ActivityMainBinding
@@ -13,14 +16,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inicializace ViewBinding
+     // Inicializace ViewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Nastavení akce pro tlačítko Toast
+       /* // Nastavení akce pro tlačítko Toast
         binding.btnShowToast.setOnClickListener {
             Toast.makeText(this, "Nazdar - mám hlad", Toast.LENGTH_LONG).show()
+        }*/
+
+
+        // Nastavení akce pro tlačítko Toast
+        binding.btnShowToast.setOnClickListener {
+            showCustomToast("Nazdar - mám hlad")
         }
+    }
+
+    // Funkce pro vlastní Toast s ikonkou
+    private fun showCustomToast(message: String) {
+        // Nafouknutí vlastního layoutu pro Toast
+        val inflater = LayoutInflater.from(this)
+        val layout = inflater.inflate(R.layout.custom_toast, null)
+
+        // Nastavení textu a ikony
+        val toastMessage: TextView = layout.findViewById(R.id.toast_message)
+        val toastIcon: ImageView = layout.findViewById(R.id.toast_icon)
+        toastMessage.text = message
+        toastIcon.setImageResource(R.drawable.ikona) // Použití ikony z drawable
+
+        // Vytvoření a zobrazení Toastu
+        val toast = Toast(this)
+        toast.duration = Toast.LENGTH_LONG
+        toast.view = layout
+        toast.show()
+
+
 
         // Nastavení akce pro tlačítko Snackbar
         binding.btnShowSnackbar.setOnClickListener {
@@ -39,6 +69,9 @@ class MainActivity : AppCompatActivity() {
 
             // Zobrazení Snackbaru
             snackbar.show()
+
         }
     }
-}
+
+
+    }

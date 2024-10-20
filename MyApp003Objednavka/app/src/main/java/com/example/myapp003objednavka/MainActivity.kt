bf8f37a33cc0a470b04f06fe8fe67fa2d303a6a9
Gabriela <gabriela.janova@tul.cz>
2024-10-20 18:07:49 +0200

    #3 přidání toastu a snackbaru

--- MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
+++ MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.myapp003objednavka
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 import com.example.myapp003objednavka.databinding.ActivityMainBinding
-
-
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +24,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     title = "Objednávka jídla"
+
+        // Zobrazení Toastu při změně vybraného RadioButton
+        binding.rgTestoviny.setOnCheckedChangeListener { _, checkedId ->
+            val selectedRadioButton = findViewById<RadioButton>(checkedId)
+            val selectedText = selectedRadioButton.text.toString()
+
+            // Vytvoření Toastu
+            Toast.makeText(this, "Vybrali jste: $selectedText", Toast.LENGTH_SHORT).show()
+        }
 
     binding.btnObjednat.setOnClickListener{
         //načtení ID vybraného radiobuttonu z radiogroup
@@ -37,7 +49,7 @@ class MainActivity : AppCompatActivity() {
                 (if(dezert)"; dezert" else "") +
                 (if(piti)"; piti" else "")
 
-                    binding.tvSouhrn.text = objednavkaText
+        binding.tvSouhrn.text = objednavkaText
 
     }
 
@@ -54,6 +66,25 @@ class MainActivity : AppCompatActivity() {
         binding.rbTestoviny3.setOnClickListener{
             binding.ivTestoviny.setImageResource(R.drawable.zapecene_testoviny_se_smetanou)
         }
+
+        //Zobrazení snackbaru
+        binding.btnObjednat.setOnClickListener {
+            // Přidáme logiku pro zobrazení Snackbaru
+            val snackbar = Snackbar.make(it, "Objednávka byla odeslána!", Snackbar.LENGTH_LONG)
+
+            // Přidáme akci pro zavření Snackbaru
+            snackbar.setAction("Zavřít") {
+                Toast.makeText(this, "Snackbar zavřen", Toast.LENGTH_SHORT).show()
+            }
+
+            // Změna barvy pozadí
+            snackbar.setBackgroundTint(resources.getColor(R.color.primary))
+
+            // Zobrazíme Snackbar
+            snackbar.show()
+        }
+
+
 
 
     }

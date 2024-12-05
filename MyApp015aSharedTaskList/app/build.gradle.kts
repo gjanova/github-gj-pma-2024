751b9a291a8b56c63c95526cd736f49444cfe614
Gabriela <gabriela.janova@tul.cz>
2024-12-05 11:14:40 +0100

    #13 začátek

--- MyApp015aSharedTaskList/app/build.gradle.kts
+++ MyApp015aSharedTaskList/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.myapp015asharedtasklist"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myapp015asharedtasklist"
@@ -33,7 +33,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
+
+
 
 dependencies {
 
